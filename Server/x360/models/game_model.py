a5adeca16623e2461b65bb336438a38b6a26a1d3
ostojan <ostojan@outlook.com>
2017-02-06 22:06:00 +0100

    Update of GameModel

--- Server/x360/models/game_model.py
+++ Server/x360/models/game_model.py
@@ -7,6 +7,13 @@ class GameModel(models.Model):
     cover_link = models.URLField(verbose_name=_('URL to cover image'))
     store_link = models.URLField(unique=True, verbose_name=_('URL to Microsoft Store'))
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.full_clean()
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return '{0}'.format(str(self.title))
+
     class Meta:
         verbose_name = _('Game')
         verbose_name_plural = _('Games')
