4995e980fdd841ab617b5b7656c3c3ed708ff32b
ostojan <ostojan@outlook.com>
2017-02-07 21:28:28 +0100

    Add GameModelApiViewSet

---
+++ Server/x360/views/api/__init__.py
@@ -0,0 +1,3 @@
+from .game_model_api_view_set import GameModelApiViewSet
+
+__all__ = ['GameModelApiViewSet']
---
+++ Server/x360/views/api/game_model_api_view_set.py
@@ -0,0 +1,15 @@
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from x360.models import GameModel
+from x360.serializers import GameModelDetailSerializer, GameModelListSerializer
+
+
+class GameModelApiViewSet(ReadOnlyModelViewSet):
+    queryset = GameModel.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return GameModelListSerializer
+        elif self.action == 'retrieve':
+            return GameModelDetailSerializer
+        raise ValueError
