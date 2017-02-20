320a83862884495e19fc2c10e0f3874ad61de531
ostojan <ostojan@outlook.com>
2017-02-20 21:45:23 +0100

    Tests for Game wrapper

---
+++ AndroidApp/app/src/test/java/com/ostojan/x360/model/GameHelper.java
@@ -0,0 +1,54 @@
+package com.ostojan.x360.model;
+
+import java.net.MalformedURLException;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by ostojan on 20.02.2017.
+ */
+
+public class GameHelper {
+
+    public static final Integer ID = 1;
+    public static final String TITLE = "Game";
+    public static final String COVER_LINK = "http://www.game.com/cover.jpg";
+    public static final String STORE_LINK = "http://www.game.com/store/game/";
+
+    public static Game createSimpleGame() throws MalformedURLException {
+        return createGameFromData(ID, TITLE, COVER_LINK, STORE_LINK);
+    }
+
+    public static  Game createSimpleGameWithoutId() throws MalformedURLException {
+        return createGameFromData(null, TITLE, COVER_LINK, STORE_LINK);
+    }
+
+    public static  Game createSimpleGameWithoutTitle() throws MalformedURLException {
+        return createGameFromData(ID, null, COVER_LINK, STORE_LINK);
+    }
+
+    public static  Game createSimpleGameWithoutCoverLink() throws MalformedURLException {
+        return createGameFromData(ID, TITLE, null, STORE_LINK);
+    }
+
+    public static  Game createSimpleGameWithoutStoreLink() throws MalformedURLException {
+        return createGameFromData(ID, TITLE, COVER_LINK, null);
+    }
+
+    public static  Game createGameFromData(Integer id, String title, String coverLink, String storeLink) throws MalformedURLException {
+        Game.Builder gameBuilder = new Game.Builder();
+        if (id != null) {
+            gameBuilder.id(id);
+        }
+        if (title != null) {
+            gameBuilder.title(title);
+        }
+        if (coverLink != null) {
+            gameBuilder.coverLink(coverLink);
+        }
+        if (storeLink != null) {
+            gameBuilder.storeLink(storeLink);
+        }
+        return gameBuilder.build();
+    }
+}
---
+++ AndroidApp/app/src/test/java/com/ostojan/x360/model/GameTest.java
@@ -0,0 +1,130 @@
+package com.ostojan.x360.model;
+
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Test
+    public void checkGetters() {
+        try {
+            tryCheckGetters();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCheckGetters() throws MalformedURLException {
+        Game game = GameHelper.createSimpleGame();
+        assertEquals("ID is not correct", GameHelper.ID, game.getId());
+        assertEquals("Title is not correct", GameHelper.TITLE, game.getTitle());
+        assertEquals("Cover's link is not correct", new URL(GameHelper.COVER_LINK), game.getCoverLink());
+        assertEquals("Store's link is not correct", new URL(GameHelper.STORE_LINK), game.getStoreLink());
+    }
+
+    @Test
+    public void compareTwoSameGames() {
+        try {
+            tryCompareTwoSameGames();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCompareTwoSameGames() throws MalformedURLException {
+        Game game1 = GameHelper.createSimpleGame();
+        Game game2 = GameHelper.createSimpleGame();
+        assertEquals("Games are not equals", game1, game2);
+        assertEquals("Games are not equals", game2, game1);
+    }
+
+    @Test
+    public void compareGameToItself() {
+        try {
+            tryCompareGameToItself();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCompareGameToItself() throws MalformedURLException {
+        Game game = GameHelper.createSimpleGame();
+        assertEquals("Game is not equals to itself", game, game);
+    }
+
+    @Test
+    public void compareTwoGamesWithDifferentIds() {
+        try {
+            tryCompareTwoGamesWithDifferentIds();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCompareTwoGamesWithDifferentIds() throws MalformedURLException {
+        Integer id1 = 1;
+        Integer id2 = 2;
+        Game game1 = GameHelper.createGameFromData(id1, GameHelper.TITLE, GameHelper.COVER_LINK, GameHelper.STORE_LINK);
+        Game game2 = GameHelper.createGameFromData(id2, GameHelper.TITLE, GameHelper.COVER_LINK, GameHelper.STORE_LINK);
+        assertNotEquals("Games are equals", game1, game2);
+        assertNotEquals("Games are equals", game2, game1);
+    }
+
+    @Test
+    public void compareTwoGamesWithDifferentTitles() {
+        try {
+            tryCompareTwoGamesWithDifferentTitles();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCompareTwoGamesWithDifferentTitles() throws MalformedURLException {
+        String title1 = "Game1";
+        String title2 = "Game2";
+        Game game1 = GameHelper.createGameFromData(GameHelper.ID, title1, GameHelper.COVER_LINK, GameHelper.STORE_LINK);
+        Game game2 = GameHelper.createGameFromData(GameHelper.ID, title2, GameHelper.COVER_LINK, GameHelper.STORE_LINK);
+        assertNotEquals("Games are equals", game1, game2);
+        assertNotEquals("Games are equals", game2, game1);
+    }
+
+    @Test
+    public void compareTwoGamesWithDifferentCoverLinks() {
+        try {
+            tryCompareTwoGamesWithDifferentCoverLinks();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCompareTwoGamesWithDifferentCoverLinks() throws MalformedURLException {
+        String coverLink1 = "http://www.game.com/cover1.jpg";
+        String coverLink2 = "http://www.game.com/cover2.jpg";
+        Game game1 = GameHelper.createGameFromData(GameHelper.ID, GameHelper.TITLE, coverLink1, GameHelper.STORE_LINK);
+        Game game2 = GameHelper.createGameFromData(GameHelper.ID, GameHelper.TITLE, coverLink2, GameHelper.STORE_LINK);
+        assertNotEquals("Games are equals", game1, game2);
+        assertNotEquals("Games are equals", game2, game1);
+    }
+
+    @Test
+    public void compareTwoGamesWithDifferentStoreLinks() {
+        try {
+            tryCompareTwoGamesWithDifferentStoreLinks();
+        } catch (MalformedURLException e) {
+            fail("Couldn't create URL for Game object");
+        }
+    }
+
+    private void tryCompareTwoGamesWithDifferentStoreLinks() throws MalformedURLException {
+        String storeLink1 = "http://www.game.com/store/game1/";
+        String storeLink2 = "http://www.game.com/store/game2/";
+        Game game1 = GameHelper.createGameFromData(GameHelper.ID, GameHelper.TITLE, GameHelper.COVER_LINK, storeLink1);
+        Game game2 = GameHelper.createGameFromData(GameHelper.ID, GameHelper.TITLE, GameHelper.COVER_LINK, storeLink2);
+        assertNotEquals("Games are equals", game1, game2);
+        assertNotEquals("Games are equals", game2, game1);
+    }
+}
