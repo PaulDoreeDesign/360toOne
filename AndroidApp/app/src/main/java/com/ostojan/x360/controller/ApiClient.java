6a13d88b66bdd7e46acfccc0880b2de0ed02f39e
ostojan <ostojan@outlook.com>
2017-02-22 19:52:55 +0100

    Update of ApiClient

--- AndroidApp/app/src/main/java/com/ostojan/x360/controller/ApiClient.java
+++ AndroidApp/app/src/main/java/com/ostojan/x360/controller/ApiClient.java
@@ -1,12 +1,43 @@
 package com.ostojan.x360.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.ostojan.x360.Properties;
+import com.ostojan.x360.model.Game;
+import com.ostojan.x360.model.GameTypeAdapter;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
 
-    public ApiClientInterface getApiClient() {
-        return null;
+    private static volatile ApiClient instance;
+    private ApiClientInterface apiClient;
+
+    private ApiClient() {
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() to get ApiClient");
+        }
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Game.class, new GameTypeAdapter())
+                .create();
+        apiClient = new Retrofit.Builder()
+                .baseUrl(Properties.SERVER_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(ApiClientInterface.class);
+    }
+
+    public ApiClientInterface getApiClientInterface() {
+        return apiClient;
     }
 
     public static ApiClient getInstance() {
-        return null;
+        if (instance == null) {
+            synchronized (ApiClient.class) {
+                if (instance == null) instance = new ApiClient();
+            }
+        }
+        return instance;
     }
 }
