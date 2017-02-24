859b686048312f944bceedadf99232fc46ed411c
ostojan <ostojan@outlook.com>
2017-02-24 22:31:22 +0100

    GameDetailActivity

---
+++ AndroidApp/app/src/main/java/com/ostojan/x360/activities/GameDetailActivity.java
@@ -0,0 +1,15 @@
+package com.ostojan.x360.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ostojan.x360.R;
+
+public class GameDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_detail);
+    }
+}
