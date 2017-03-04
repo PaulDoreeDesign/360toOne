01bdf9b2656f18eef9ca469dfa84de493eb365fe
ostojan <ostojan@outlook.com>
2017-03-04 21:08:37 +0100

    Update of RegionModel

--- Server/x360/models/region_model.py
+++ Server/x360/models/region_model.py
@@ -4,7 +4,15 @@ from django.utils.translation import gettext_lazy as _
 
 class RegionModel(models.Model):
     name = models.CharField(max_length=255, verbose_name=_('Name'))
-    code = models.CharField(max_length=2, verbose_name=_('Region code'))
+    code = models.CharField(max_length=2, unique=True, verbose_name=_('Region code'))
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.code = self.code.lower()
+        self.full_clean()
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return '{0} ({1})'.format(str(self.name), str(self.code))
 
     class Meta:
         verbose_name = _('Region')
