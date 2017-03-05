7e3df6bbd1bcdfde1123b9fd259a4b316e7aee8d
ostojan <ostojan@outlook.com>
2017-03-05 20:08:54 +0100

    Add support for Regions in Games downloading mechanism

--- Server/x360/admin/game_model_admin.py
+++ Server/x360/admin/game_model_admin.py
@@ -11,7 +11,7 @@ from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _, ngettext_lazy as __
 
-from x360.models import GameModel
+from x360.models import GameModel, RegionModel
 
 
 @register(GameModel)
@@ -31,7 +31,8 @@ class GameModelAdmin(admin.ModelAdmin):
                                       plural_name=GameModel._meta.verbose_name_plural
                                   ),
                                   level=messages.INFO)
-            except (ValueError, AttributeError):
+            except (ValueError, AttributeError) as e:
+                print(e)
                 self.message_user(request, _('There was problem with downloaded data'), level=messages.ERROR)
             except gaierror:
                 self.message_user(request, _('Encountered problem connection external server'), level=messages.ERROR)
@@ -45,7 +46,18 @@ class GameModelAdmin(admin.ModelAdmin):
         added_games = 0
         for game in games_from_xbox:
             try:
-                GameModel.objects.create(**game)
+                game_dict = dict(title=game['title'].lstrip().rstrip(), cover_link=game['cover_link'], store_link=game['store_link'])
+                game_object = GameModel.objects.create(**game_dict)
+                available_regions_codes = [code.lstrip().rstrip().lower() for code in
+                                           game['available_regions'].lstrip().rstrip().split(',') if code != '']
+                available_regions = RegionModel.objects.filter(code__in=available_regions_codes)
+                for region in available_regions:
+                    game_object.available_regions.add(region)
+                excluded_regions_codes = [code.lstrip().rstrip().lower() for code in
+                                          game['excluded_regions'].lstrip().rstrip().split(',') if code != '']
+                excluded_regions = RegionModel.objects.filter(code__in=excluded_regions_codes)
+                for region in excluded_regions:
+                    game_object.excluded_regions.add(region)
             except ValidationError:  # Validation error is raised if Game is already in database
                 pass
             else:
@@ -70,16 +82,18 @@ class GameModelAdmin(admin.ModelAdmin):
         json_string = re.sub('url(:.*?(?P<start>[\'"])(?P<name>.*?)(?P=start)[.\s]*?(?P<after>[,}\]]))',
                              '"store_link":"\g<name>"\g<after>',
                              json_string)
+        json_string = re.sub('includedLoc:(.*?(?P<start>[\'"])(?P<value>.*?)(?P=start)[.\s]*?)(?P<after>[,}\]])',
+                             '"available_regions":"\g<value>"\g<after>', json_string)
+        json_string = re.sub('excludedLoc:(.*?(?P<start>[\'"])(?P<value>.*?)(?P=start)[.\s]*?)(?P<after>[,}\]])',
+                             '"excluded_regions":"\g<value>"\g<after>', json_string)
         return json_string
 
     def remove_unused_fields(self, json_string):
-        json_string = re.sub('((includedLoc)|(excludedLoc)):(.*?(?P<start>[\'"]).*?(?P=start)[.\s]*?)(?P<after>[,}\]])',
-                             '\g<after>', json_string)
         json_string = re.sub('((justreleased)|(fanfav)).*?(?P<after>[,}\]])', '\g<after>', json_string)
         return json_string
 
     def fix_malformed_json(self, json_string):
-        json_string = re.sub(',[,\s]*,+', '', json_string)
+        json_string = re.sub(',[,\s]*,+', ',', json_string)
         json_string = re.sub('(?P<start>[{\[])[,\s]+', '\g<start>', json_string)
         json_string = re.sub('[,\s]+(?P<end>[}\]])', '\g<end>', json_string)
         return json_string
