409d1bce9eac2689c4588ea65f531b941e742ba8
ostojan <ostojan@outlook.com>
2017-03-05 20:18:19 +0100

    Add Regions view to urls

--- Server/x360/urls.py
+++ Server/x360/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from rest_framework.routers import SimpleRouter
 
-from x360.views.api import GameModelApiViewSet
+from x360.views.api import GameModelApiViewSet, RegionModelApiViewSet
 
 router = SimpleRouter()
 router.register(r'games', GameModelApiViewSet)
+router.register(r'regions', RegionModelApiViewSet)
 urlpatterns = [
     url(r'^api/', include(router.urls))
 ]
