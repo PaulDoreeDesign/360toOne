f3dc9ccf746c0e0a7d75136906523ed1c8ca4223
ostojan <ostojan@outlook.com>
2017-03-05 21:41:02 +0100

    RegionTypeAdapter update

--- AndroidApp/app/src/main/java/com/ostojan/x360/model/RegionTypeAdapter.java
+++ AndroidApp/app/src/main/java/com/ostojan/x360/model/RegionTypeAdapter.java
@@ -9,10 +9,34 @@ import java.io.IOException;
 public class RegionTypeAdapter extends TypeAdapter<Region> {
     @Override
     public void write(JsonWriter out, final Region region) throws IOException {
+        out.beginObject();
+        out.name("id").value(region.getId());
+        out.name("name").value(region.getName());
+        out.name("code").value(region.getCode());
+        out.endObject();
     }
 
     @Override
     public Region read(JsonReader in) throws IOException {
-        return null;
+        Region.Builder regionBuilder = new Region.Builder();
+        in.beginObject();
+        while (in.hasNext()) {
+            switch (in.nextName()) {
+                case "id":
+                    regionBuilder.id(in.nextInt());
+                    break;
+                case "name":
+                    regionBuilder.name(in.nextString());
+                    break;
+                case "code":
+                    regionBuilder.code(in.nextString());
+                    break;
+                default:
+                    in.skipValue();
+                    break;
+            }
+        }
+        in.endObject();
+        return regionBuilder.build();
     }
 }
