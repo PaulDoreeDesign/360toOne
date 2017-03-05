8c5c1d53e6a1ce43ef0e19bb5a3dc03dde7d570d
ostojan <ostojan@outlook.com>
2017-03-05 20:11:43 +0100

    Add support for Regions in GameModelDetialSerializer

--- Server/x360/serializers/game_model_detail_serializer.py
+++ Server/x360/serializers/game_model_detail_serializer.py
@@ -6,4 +6,4 @@ from x360.models import GameModel
 class GameModelDetailSerializer(ModelSerializer):
     class Meta:
         model = GameModel
-        fields = ('id', 'title', 'cover_link', 'store_link')
+        fields = ('id', 'title', 'cover_link', 'store_link', 'available_regions', 'excluded_regions')
