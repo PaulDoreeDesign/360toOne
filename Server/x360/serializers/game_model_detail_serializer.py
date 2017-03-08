ea9746f2cbc535dca87498ae7bcd53ba536ea458
ostojan <ostojan@outlook.com>
2017-03-08 20:56:28 +0100

    Add RegionModel serializer to GameModelDetialSerializer

--- Server/x360/serializers/game_model_detail_serializer.py
+++ Server/x360/serializers/game_model_detail_serializer.py
@@ -1,9 +1,13 @@
 from rest_framework.serializers import ModelSerializer
 
+from .region_model_serializer import RegionModelSerializer
 from x360.models import GameModel
 
 
 class GameModelDetailSerializer(ModelSerializer):
+    available_regions = RegionModelSerializer(many=True, read_only=True)
+    excluded_regions = RegionModelSerializer(many=True, read_only=True)
+
     class Meta:
         model = GameModel
         fields = ('id', 'title', 'cover_link', 'store_link', 'available_regions', 'excluded_regions')
